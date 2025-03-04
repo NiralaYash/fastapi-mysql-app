341a2ab4ca514e2b73731ae7938bf27e86e4b12a
NiralaYash <niralayash@gmail.com>
2025-03-04 15:00:56 +0530

    Added Create, Read and Delete operations for 'songs' and 'artists'

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ def get_db():
 
 db_dependency = Annotated[Session,Depends(get_db)]
 
+
+## Song CRUD Operations
 @app.post("/songs/", status_code=status.HTTP_201_CREATED)
 async def create_song(song: SongBase, db: db_dependency):
     db_song = models.Song(**song.model_dump())
@@ -37,8 +39,37 @@ async def read_song(song_Id: int, db: db_dependency):
         HTTPException(status_code=404, detail='Song was not found')
     return song
 
+# TODO Song Update
+
+@app.delete("/songs/{song_Id}", status_code= status.HTTP_200_OK)
+async def delete_song(song_Id: int, db: db_dependency):
+    db_song = db.query(models.Song).filter(models.Song.id == song_Id).first()
+    if db_song is None:
+        raise HTTPException(status_code=404, detail='Song was not found')
+    db.delete(db_song)
+    db.commit()
+
+
+## Artist CRUD Operations
 @app.post("/artists/", status_code=status.HTTP_201_CREATED)
 async def create_artist(artist: ArtistBase, db: db_dependency):
     db_artist = models.Artist(**artist.model_dump())
     db.add(artist)
+    db.commit()
+
+@app.get("/artists/{artist_Id}", status_code= status.HTTP_200_OK)
+async def read_artist(artist_Id: int, db: db_dependency):
+    artist = db.query(models.Artist).filter(models.Artist.id == artist_Id).first()
+    if artist is None:
+        raise HTTPException(status_code=404, detail='Artist was not found')
+    return artist
+
+# TODO Artist Update
+
+@app.delete("/artists/{artist_Id}", status_code= status.HTTP_200_OK)
+async def delete_artist(artist_Id: int, db: db_dependency):
+    db_artist = db.query(models.Artist).filter(models.Artist.id == artist_Id).first()
+    if db_artist is None:
+        raise HTTPException(status_code=404, detail='Artist was not found')
+    db.delete(db_artist)
     db.commit()
